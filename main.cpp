d88a5f9d15c17c4cfc9ba0b09cc8d02082efd361
hihikomori <ghhhhhnika@gmail.com>
2025-11-01 19:48:25 +0300

    set up commit

--- main.cpp
+++ main.cpp
@@ -3,65 +3,39 @@
 #include <iostream>
 #include <new>
 
-void rm(int **mtx, int rows);
+void rm(int **mtx, size_t rows);
 
-int **makeMtx(int rows, int cols) {
-  int **mtx = new int *[rows];
-  for (size_t i = 0; i < rows; ++i) {
-    try {
-      mtx[i] = new int[cols];
-    } catch (std::bad_alloc &e) {
-      rm(mtx, i);
-      throw;
-    }
+int **makeMtx(const size_t * lns, size_t rows) {
+  
+}
+
+void input(int **mtx, const size_t * lns, int rows, const int * t, size_t n) {
+  
+}
+
+void output(int ** mtx, size_t rows, const size_t * lns) {
+  
+}
+
+void rm(int **mtx, size_t rows) {
+  
+}
+
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows){
+  int ** mtx = nullptr;
+  try{
+    mtx = makeMtx(lns, rows);
+  }catch(std::bad_alloc){
+    throw;
   }
+  input(mtx, lns, rows, t, n);
+	output(mtx, rows, lns);
+	for(int i = 0; i < 15; ++i){
+		std::cout << t[i] << " ";
+	}
   return mtx;
 }
 
-void input(int **mtx, int rows, int cols) {
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      std::cin >> mtx[i][j];
-    }
-  }
-}
-
-void output(const int *const *mtx, int rows, int cols) {
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      std::cout << mtx[i][j];
-    }
-    std::cout << std::endl;
-  }
-}
-
-void rm(int **mtx, int rows) {
-  for (size_t i = 0; i < rows; ++i) {
-    delete[] mtx[i];
-  }
-  delete[] mtx;
-}
-
 int main() {
-  int rows = 0, cols = 0;
-  std::cin >> rows >> cols;
-  if ((std::cin.fail())) {
-    return 1;
-  }
-  int **mtx = nullptr;
-  try {
-    mtx = makeMtx(rows, cols);
-  } catch (const std::bad_alloc &e) {
-    std::cerr << e.what();
-    return 2;
-  }
-
-  input(mtx, rows, cols);
-  if (std::cin.bad()) {
-    std::cerr << "Error, bad input";
-    rm(mtx, rows);
-    return 1;
-  }
-  output(mtx, rows, cols);
-  rm(mtx, rows);
+  
 }
