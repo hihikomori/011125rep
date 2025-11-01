bc210164901734e80edb8d7fe82d44be3265e5bd
hihikomori <ghhhhhnika@gmail.com>
2025-11-01 20:19:52 +0300

    done task

--- main.cpp
+++ main.cpp
@@ -6,19 +6,45 @@
 void rm(int **mtx, size_t rows);
 
 int **makeMtx(const size_t * lns, size_t rows) {
-  
+  int **mtx = new int *[rows];
+  for (size_t i = 0; i < rows; ++i) {
+    try {
+      mtx[i] = new int[lns[i]];
+    } catch (std::bad_alloc &e) {
+      rm(mtx, i);
+      throw;
+    }
+  }
+  return mtx;
 }
 
 void input(int **mtx, const size_t * lns, int rows, const int * t, size_t n) {
-  
+  size_t cnt = 0;
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < lns[i]; ++j) {
+      mtx[i][j] = t[cnt];
+			++cnt;
+      if(cnt >= n){
+        break;
+      }
+    }
+  }
 }
 
 void output(int ** mtx, size_t rows, const size_t * lns) {
-  
+  for(int i = 0; i < rows; ++i){
+    for (size_t j = 0; j < lns[i]; ++j) {
+      std::cout << mtx[i][j] << ' ';
+    }
+	  std::cout << '\n';
+	}
 }
 
 void rm(int **mtx, size_t rows) {
-  
+  for (size_t i = 0; i < rows; ++i) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
 }
 
 int ** convert(const int * t, size_t n, const size_t * lns, size_t rows){
@@ -30,12 +56,26 @@ int ** convert(const int * t, size_t n, const size_t * lns, size_t rows){
   }
   input(mtx, lns, rows, t, n);
 	output(mtx, rows, lns);
-	for(int i = 0; i < 15; ++i){
-		std::cout << t[i] << " ";
-	}
   return mtx;
 }
 
 int main() {
-  
+  int exit_code = 0;
+  int ** mtx = nullptr;
+  size_t n = 12;
+  int * t = new int[n]{5,5,5,5,6,6,7,7,7,7,7,8};
+  size_t rows = 4;
+  size_t * lns = new size_t[rows]{4,2,5,1};
+  try{
+    mtx = convert(t, n, lns, rows);
+  }catch(std::bad_alloc &e){
+    exit_code = 2;
+  }
+  catch(...){
+    exit_code = 3;
+  }
+  delete[] t;
+  delete[] lns;
+  if (mtx) {rm(mtx, rows);}
+  return exit_code;
 }
